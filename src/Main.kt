aa5bc1066a4ea87ceb394d6dc368c37050ecf7e7
shotx <pokerking123@inbox.ru>
2021-03-28 16:45:23 +0400

    update

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,21 @@
 fun main() {
-    print(nNumber(327))
+    print(nNumber(-1))
 }
 
 fun nNumber(n: Int): Int {
+    if (n==0) return 0
     var sumNumber = n
     var multNumber = n
     var sum = 0
     var multiply = 1
-    while (sumNumber > 0) {
+    while (sumNumber != 0) {
         val r = sumNumber % 10
         sum += r
         sumNumber /= 10
     }
     while (multNumber != 0) {
         val r = multNumber % 10
-        multiply= multiply * r
+        multiply *= r
         multNumber /= 10
     }
     return multiply- sum
